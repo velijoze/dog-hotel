1ce8dfe369da62c844df6285f0317352471feb30
velijoze <velijoze@outlook.com>
2017-11-09 16:20:58 +0000

    Add about, daycare, boarding, training pages and adjust links in navbar

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,17 @@ class MainController < ApplicationController
 
   def index
   end
+
+  def about
+  end
+
+  def daycare
+  end
+
+  def boarding
+  end
+
+  def training
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :dog_registrations
   resources :users
   root 'main#index'
+  get 'about', to: 'main#about'
+  get 'daycare', to: 'main#daycare'
+  get 'boarding', to: 'main#boarding'
+  get 'training', to: 'main#training'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
